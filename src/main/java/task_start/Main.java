876011b8c773fdcf56dbf7ac81fff0e21abdb8c4
Andrei Markov <noteandmark@gmail.com>
2022-02-21 17:35:15 +0200

    add task1

---
+++ src/main/java/task_start/Main.java
@@ -0,0 +1,9 @@
+package main.java.task_start;
+
+import static main.java.task1_stringsendswith.Kata.solution;
+
+public class Main {
+    public static void main(String[] args) {
+        solution("abc","ab");
+    }
+}
