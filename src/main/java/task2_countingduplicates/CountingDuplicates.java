40e6b0c1a53688f16530bab3c03909b2f6e3f002
Andrei Markov <noteandmark@gmail.com>
2022-02-22 14:06:35 +0200

    add 2nd task

---
+++ src/main/java/task2_countingduplicates/CountingDuplicates.java
@@ -0,0 +1,27 @@
+package task2_countingduplicates;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CountingDuplicates {
+    public static int duplicateCount(String text) {
+        int result = 0;
+        LinkedHashMap<Character, Integer> mapOfUniqueCharacters = new LinkedHashMap<Character, Integer>();
+
+        for (Character character : text.toCharArray()) {
+            char symbol = Character.toLowerCase(character);
+            mapOfUniqueCharacters.put(
+                    symbol,
+                    (mapOfUniqueCharacters.get(symbol) == null) ? 1 : mapOfUniqueCharacters.get(symbol) + 1);
+        }
+
+        for (Map.Entry entry: mapOfUniqueCharacters.entrySet()) {
+            int value = (int) entry.getValue();
+            if (value>1) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
