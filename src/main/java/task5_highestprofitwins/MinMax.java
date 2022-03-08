100e59c0e78510535f53de458850e6363aa2b301
Andrei Markov <noteandmark@gmail.com>
2022-03-08 21:18:43 +0200

    add task5

---
+++ src/main/java/task5_highestprofitwins/MinMax.java
@@ -0,0 +1,13 @@
+package task5_highestprofitwins;
+
+import java.util.Arrays;
+
+public class MinMax {
+    public static int[] minMax(int[] arr) {
+        int[] result = new int [2];
+        Arrays.sort(arr);
+        result[0] = arr[0];
+        result[1] = arr[arr.length-1];
+        return result;
+    }
+}
