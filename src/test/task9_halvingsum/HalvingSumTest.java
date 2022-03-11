4476dc1e2163076e42c72f946b61c76330b5b234
Andrei Markov <noteandmark@gmail.com>
2022-03-11 22:22:41 +0200

    add tasks 9

---
+++ src/test/task9_halvingsum/HalvingSumTest.java
@@ -0,0 +1,13 @@
+package task9_halvingsum;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HalvingSumTest {
+    @Test
+    public void test() {
+        HalvingSum hs = new HalvingSum();
+        assertEquals(47,hs.halvingSum(25));
+        assertEquals(247,hs.halvingSum(127));
+    }
+}
