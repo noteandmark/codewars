8c0114d9ef94aa1418728e248bf8a6efaae5ba9e
Andrei Markov <noteandmark@gmail.com>
2023-07-03 18:01:02 +0300

    added class versionmanager to task33

--- src/main/java/tasks_6_level/task33_versionsmanager/VersionManager.java
+++ src/main/java/tasks_6_level/task33_versionsmanager/VersionManager.java
@@ -2,4 +2,28 @@ package tasks_6_level.task33_versionsmanager;
 
 public class VersionManager {
 
+	public VersionManager(String string) {
+
+	}
+	
+	public void major() {
+
+	}
+	
+	public void minor() {
+
+	}
+
+	public void patch() {
+		
+	}
+	
+	public void rollback() {
+		
+	}
+	
+	public String release() {
+		return null;		
+	}
+
 }
