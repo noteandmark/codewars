5ed25f50df85083d5e3ce1c65ae2b9234f602846
Andrei Markov <noteandmark@gmail.com>
2023-07-11 23:07:38 +0300

    complete task35

--- src/main/java/tasks_5_level/task35_startingstrings/TreeJava.java
+++ src/main/java/tasks_5_level/task35_startingstrings/TreeJava.java
@@ -1,7 +1,26 @@
 package tasks_5_level.task35_startingstrings;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+
 public class TreeJava {
 	  public static String[] solution(String str) {
-		    return null;
+		    HashMap<String, List<String>> startsWith = new HashMap<>();
+
+		    for (String word : str.split("\\s+")) {
+		      word = word.toLowerCase();
+		      for (int i=0; i<word.length(); i++) {
+		        List<String> list = startsWith.getOrDefault(word.substring(0,i+1), new ArrayList<String>());
+		        list.add(word);
+		        startsWith.putIfAbsent(word.substring(0,i+1), list);
+		      }
+		    }
+		    
+		  return startsWith.entrySet().stream()
+		      .filter(e -> e.getKey().length()==e.getValue().size())
+		      .flatMap(e -> e.getValue().stream())
+		      .sorted()
+		      .toArray(String[]::new);
 		  }
 }
