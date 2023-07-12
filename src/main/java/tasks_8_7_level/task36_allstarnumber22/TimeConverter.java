178c7681223449a6a552479f831bbea685a7e324
Andrei Markov <noteandmark@gmail.com>
2023-07-12 18:06:15 +0300

    complete task36

--- src/main/java/tasks_8_7_level/task36_allstarnumber22/TimeConverter.java
+++ src/main/java/tasks_8_7_level/task36_allstarnumber22/TimeConverter.java
@@ -1,7 +1,11 @@
 package tasks_8_7_level.task36_allstarnumber22;
 
 public class TimeConverter {
-	  public static String toTime(int seconds){
-		return null;
-		  }
+	public static String toTime(int seconds) {
+		int hours;
+		int minutes;
+		hours = (int) Math.floor(seconds / 3600);
+		minutes = (int) Math.floor((seconds - hours * 3600) / 60);
+		return hours + " hour(s) and " + minutes + " minute(s)";
+	}
 }
