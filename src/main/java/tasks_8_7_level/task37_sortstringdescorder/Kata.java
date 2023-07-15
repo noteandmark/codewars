05fbcee8c59634d038e23bcb01daa3ef582cf73c
Andrei Markov <noteandmark@gmail.com>
2023-07-15 13:20:30 +0300

    added new task37

---
+++ src/main/java/tasks_8_7_level/task37_sortstringdescorder/Kata.java
@@ -0,0 +1,9 @@
+package tasks_8_7_level.task37_sortstringdescorder;
+
+public class Kata {
+	  public static String[] lineupStudents(String students)
+	    {     
+	      String[] res = new String[]; 
+	      return res;
+	    }
+}
