0dd86340ae68a8b1bdb7052fd31b77d6a47f7e12
Andrei Markov <noteandmark@gmail.com>
2023-07-19 18:16:06 +0300

    complete task40

--- src/main/java/tasks_8_7_level/task40_leapyears/LeapYears.java
+++ src/main/java/tasks_8_7_level/task40_leapyears/LeapYears.java
@@ -2,6 +2,6 @@ package tasks_8_7_level.task40_leapyears;
 
 public class LeapYears {
 	public static boolean isLeapYear(int year) {
-		return false;
+		return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
 	}
 }
