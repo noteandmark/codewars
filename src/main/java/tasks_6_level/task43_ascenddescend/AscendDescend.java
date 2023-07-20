55e6fd1d2a91ac5f4a3c23bb108e319cfedd8c13
Andrei Markov <noteandmark@gmail.com>
2023-07-20 17:23:28 +0300

    complete new task43

--- src/main/java/tasks_6_level/task43_ascenddescend/AscendDescend.java
+++ src/main/java/tasks_6_level/task43_ascenddescend/AscendDescend.java
@@ -2,11 +2,27 @@ package tasks_6_level.task43_ascenddescend;
 
 public class AscendDescend {
     public static String ascendDescend(int length, int minimum, int maximum) {
-        String result = null;
-        StringBuilder sb = new StringBuilder();
-        for (int i = minimum; i <= maximum; i++) {
-
+        if (length <= 0 || maximum < minimum) {
+            return "";
         }
-        return result;
+
+        StringBuilder result = new StringBuilder();
+        int ref = minimum;
+        int increment = 1;
+
+        while (result.length() < length) {
+            result.append(ref);
+
+            if (result.length() == length) {
+                break;
+            }
+            if (minimum != maximum) ref += increment;
+
+            if ((increment > 0 && ref >= maximum) || (increment < 0 && ref <= minimum)) {
+                increment *= -1;
+            }
+        }
+
+        return result.substring(0, length);
     }
 }
--- src/test/tasks_6_level/task43_ascenddescend/AscendDescendTest.java
+++ src/test/tasks_6_level/task43_ascenddescend/AscendDescendTest.java
@@ -9,5 +9,7 @@ class AscendDescendTest {
         assertEquals("12321", AscendDescend.ascendDescend(5, 1, 3));
         assertEquals("01210121012101", AscendDescend.ascendDescend(14, 0, 2));
         assertEquals("56789876567", AscendDescend.ascendDescend(11, 5, 9));
+        assertEquals("1111111111", AscendDescend.ascendDescend(10, 1, 1));
+        assertEquals("-9-8-7-6-5-4-3-4-5-6-7-8-", AscendDescend.ascendDescend(25, -9, -3));
     }
 }
